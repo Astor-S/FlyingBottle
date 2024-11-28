de731505d30ef20aecb2a92571f44d9c6590a746
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-11-28 17:47:44 +0300

    Update

---
+++ Assets/Scripts/BottleJumpRotate.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class BottleJumpRotate : MonoBehaviour
+{
+    [SerializeField] private KeyCode _keyJump = KeyCode.Space;
+    [SerializeField] private float _jumpHeight = 2f;
+    [SerializeField] private float _jumpDuration = 1f;
+    [SerializeField] private float _rotationDuration = 1f;
+    
+    private bool _isGrounded;
+    private bool _canDoubleJump;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_keyJump))
+        {
+            JumpAndRotate();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Surface>(out _))
+        {
+            _isGrounded = true;
+            _canDoubleJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Surface>(out _))
+        {
+            _isGrounded = false;
+        }
+    }
+
+    private void JumpAndRotate()
+    {
+        if (_isGrounded)
+        {
+            Vector3 initialPosition = transform.position;
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(transform.DOMoveY(initialPosition.y + _jumpHeight, _jumpDuration / 2f).SetEase(Ease.OutCubic));
+            sequence.Append(transform.DOMoveY(initialPosition.y, _jumpDuration / 2f).SetEase(Ease.InCubic));
+
+            Rotate(sequence);
+        }
+    }
+
+    private void Rotate(Sequence sequence)
+    {
+        sequence.Join(transform.DORotate(new Vector3(0, 360f, 0), _rotationDuration));
+    }
+}
