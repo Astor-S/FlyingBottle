4f75ca37469eed2d1285a575a97b1ae96638083f
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-12-03 17:32:38 +0300

    Update

---
+++ Assets/Scripts/BreakShelf.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BreakShelf : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _breakAnimationTrigger = "Break";
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out _))
+            _animator.SetTrigger(_breakAnimationTrigger); 
+    }
+}
