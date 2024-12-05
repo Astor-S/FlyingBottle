e442c675c3c0ec62e80c27df7a5750bf5b831dd2
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-12-05 18:35:10 +0300

    Update

--- Assets/Scripts/Player/CollisionDetector.cs
+++ Assets/Scripts/Player/CollisionDetector.cs
@@ -6,18 +6,6 @@ public class CollisionDetector : MonoBehaviour
     public event Action FailedCollide;
     public event Action FinishedCollide;
 
-    //private void OnTriggerEnter(Collider collision)
-    //{
-    //    if (collision.gameObject.TryGetComponent<Ground>(out _))
-    //        FailedCollide?.Invoke();
-
-    //    if(collision.gameObject.TryGetComponent<FinishPortal>(out _))
-    //        FinishedCollide?.Invoke();   
-        
-    //    if(collision.gameObject.TryGetComponent(out Crate crate))
-    //        transform.parent = crate.transform;  
-    //}
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent<Ground>(out _))
@@ -35,10 +23,4 @@ public class CollisionDetector : MonoBehaviour
         if (collision.gameObject.TryGetComponent<Crate>(out _))
             transform.parent = null;
     }
-
-    //private void OnTriggerExit(Collider collision)
-    //{
-    //    if (collision.gameObject.TryGetComponent <Crate>(out _))
-    //        transform.parent = null;
-    //}
 }
