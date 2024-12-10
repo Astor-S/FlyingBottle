326ce428ee97bbeb701f2faed923d3c0dfb35319
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-12-10 19:53:03 +0300

    Update

--- Assets/Scripts/Objects/Shelf/ShelfBreaker.cs
+++ Assets/Scripts/Objects/Shelf/ShelfBreaker.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 public class ShelfBreaker : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private AudioClip _shelfSound;
     [SerializeField] private string _breakAnimationTrigger = "Break";
 
+    private AudioSource _audioSource;
+
     public event Action OnShelfBreak;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,6 +22,7 @@ public class ShelfBreaker : MonoBehaviour
         {
             _animator.SetTrigger(_breakAnimationTrigger);
             OnShelfBreak?.Invoke();
+            _audioSource.PlayOneShot(_shelfSound);
         }
     }
 }
