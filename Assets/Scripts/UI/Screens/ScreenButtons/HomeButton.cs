4f642f95664ea2dd987a4b9a47ae3350adf1cc8f
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-12-19 22:37:59 +0300

    Update

--- Assets/Scripts/UI/Screens/ScreenButtons/HomeButton.cs
+++ Assets/Scripts/UI/Screens/ScreenButtons/HomeButton.cs
@@ -1,25 +1,4 @@
 using NaughtyAttributes;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class HomeButton : MonoBehaviour
-{
-    [Scene]
-    [SerializeField] private string _sceneToLoad;
-
-    private void OnButtonClick()
-    {
-        StartCoroutine(LoadHomeAsync(_sceneToLoad));
-    }
-
-    private IEnumerator LoadHomeAsync(string sceneName)
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-
-        while (asyncLoad.isDone == false)
-        {
-            yield return null;
-        }
-    }
-}
+public class HomeButton : LoadSceneButton {}
