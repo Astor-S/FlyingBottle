506ce9b3cf512d97cbd441ba5e0a50c9f2876b6f
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-12-25 16:13:17 +0300

    Added player disappearance
    Added player disappearance upon level completion

--- Assets/Scripts/FinishPortal.cs
+++ Assets/Scripts/FinishPortal.cs
@@ -1,3 +1,10 @@
 using UnityEngine;
 
-public class FinishPortal : MonoBehaviour {}
+public class FinishPortal : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player player))
+            player.gameObject.SetActive(false);   
+    }
+}
