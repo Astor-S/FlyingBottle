1e42a82f86662adfc32b3c9c159e88b2782323e0
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-01-15 14:57:38 +0300

    Optimization

--- Assets/Scripts/PlayerControlSystem/PlayerMover.cs
+++ Assets/Scripts/PlayerControlSystem/PlayerMover.cs
@@ -179,7 +179,7 @@ namespace PlayerControlSystem
             bool CheckPosition(Vector3 position)
             {
                 Vector3 direction = (position - _rigidbody.position).normalized;
-                float distance = Vector3.Distance(_rigidbody.position, position);
+                float distance = (_rigidbody.position - position).sqrMagnitude;
 
                 int count = Physics.BoxCastNonAlloc(
                     _boxCollider.bounds.center,
