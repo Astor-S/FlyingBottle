3103f2ccb3273096fc1e50bd47d23063e26cb7f4
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-01-22 15:57:53 +0300

    Added coins for watch ad

---
+++ Assets/Scripts/UI/Home/ShopMenu/AddMoneyButton.cs
@@ -0,0 +1,45 @@
+using UI.Home.Interfaces;
+using UnityEngine;
+using YG;
+
+namespace UI.Home.ShopMenu
+{
+    public class AddMoneyButton : MonoBehaviour, IMenuButton
+    {
+        private SavesYG _savesYG;
+
+        private int _coinsFofWathAD = 200;
+
+        private void Start()
+        {
+            _savesYG = YandexGame.savesData;
+        }
+
+        private void OnEnable()
+        {
+            YandexGame.RewardVideoEvent += Rewarded;
+        }
+
+        private void OnDisable()
+        {
+            YandexGame.RewardVideoEvent -= Rewarded;
+        }
+
+        public void OnButtonClick() =>
+            OpenRewardAd(0);
+
+        private void OpenRewardAd(int id) =>
+            YandexGame.RewVideoShow(id);
+
+        private void Rewarded(int id) =>
+            AddMoney();
+
+        private void AddMoney()
+        {
+            _savesYG.balanceMoney += _coinsFofWathAD;
+            YandexGame.SaveProgress();
+        }
+    }
+
+
+}
