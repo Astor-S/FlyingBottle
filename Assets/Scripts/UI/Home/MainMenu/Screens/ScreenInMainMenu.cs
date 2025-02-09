d2e61270f7f22491373153171222dbf8588b1141
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-02-09 17:35:51 +0300

    Refactoring

--- Assets/Scripts/UI/Home/MainMenu/Screens/ScreenInMainMenu.cs
+++ Assets/Scripts/UI/Home/MainMenu/Screens/ScreenInMainMenu.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,40 +9,15 @@ namespace UI.Home.MainMenu.Screens
         [SerializeField] private Button _openButton;
         [SerializeField] private Button _closeButton;
 
-        public event Action OpenButtonClicked;
-        public event Action CloseButtonClicked;
-
         private void Awake()
         {
             gameObject.SetActive(false);
         }
 
-        private void OnEnable()
-        {
-            _openButton.onClick.AddListener(OnOpenButtonClick);
-            _closeButton.onClick.AddListener(OnCloseButtonClick);
-        }
-
-        private void OnDisable()
-        {
-            _openButton.onClick.RemoveListener(OnOpenButtonClick);
-            _closeButton.onClick.RemoveListener(OnCloseButtonClick);
-        }
-
-        public void Open()
-        {
+        public void Open() =>
             gameObject.SetActive(true);
-        }
 
-        public void Close()
-        {
+        public void Close() =>
             gameObject.SetActive(false);
-        }
-
-        private void OnOpenButtonClick() =>
-            OpenButtonClicked?.Invoke();
-
-        private void OnCloseButtonClick() =>
-            CloseButtonClicked?.Invoke();
     }
 }
